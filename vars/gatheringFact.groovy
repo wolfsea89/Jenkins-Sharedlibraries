6e2464f6f1f2f30ce55839635a5153d8ea0d5759
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 23:35:33 +0100

    test

--- vars/gatheringFact.groovy
+++ vars/gatheringFact.groovy
@@ -6,14 +6,30 @@ def call(Object formParams, Object env ) {
     println(setfact)
 }
 
-def getVersion(String branch, String buildNumber]){
-    // def result = []
+def getVersion(String branch, String buildNumber){
+    def version
     String branchPrefix = getBranch(branch)
     if (branchPrefix ==~/feature|epicfeature|develop/) {
         def now = new Date()
-        println("${now.getYear()}.${now.getMonth()}.${now.getDay()}.${buildNumber}")
-        println("${now.getYear()}.${now.getMonth()}.${now.getDay()}")
+        version = "${now.getYear()}.${now.getMonth()}.${now.getDay()}",
+    } else if(branchPrefix ==~/release|hotfix|master/){
+        for(def substring in branchPrefix.split('/')){
+            if(substring ==~ /[0-9]+\.[0-9]+$/){
+                version = "${substring}.0"
+            } else if (substring ==~ /[0-9]+\.[0-9]+\.[0-9]+$/){
+                version = "${substring}.0"
+            }
+        }
     }
+    if (version != ""){
+        result = [
+            'semanticVersion': "${version}",
+            'semanticVersionWithBuildNumber': "${version}.${buildNumber}"
+        ]
+    } else {
+        error("ERROR: Branch name not compatible with gitflow. Expects value (feature/*, epicfeature/*, develop, release, release/X.Y, release/X.Y.0, hotfix, hotfix/X.Y.Z, master)")
+    }
+    println(result)
 }
 
 def getBranch(String branch) {
@@ -24,12 +40,14 @@ def getBranch(String branch) {
         result = 'epicfeature'
     } else if (branch ==~ /(.*\/develop)|(develop)$/) {
         result = 'develop'
-    } else if (branch ==~ /(.*\/release)|(release)\/.*$/) {
+    } else if (branch ==~ /(.*\/release|release)(\/([0-9]+\.[0-9]+|[0-9]+\.[0-9]+\.0)|)$/) {
         result = 'release'
-    } else if (branch ==~ /(.*\/hotfix)|(hotfix)\/.*$/) {
+    } else if (branch ==~ /(.*\/hotfix|hotfix)(\/([0-9]+\.[0-9]+\.[0-9]+)|)$/) {
         result = 'hotfix'
     } else if (branch ==~ /(.*\/master)|(master)$/) {
         result = 'master'
+    } else {
+        error("ERROR: Branch name not compatible with gitflow. Expects value (feature/*, epicfeature/*, develop, release, release/X.Y, release/X.Y.0, hotfix, hotfix/X.Y.Z, master)")
     }
     return result
 }
