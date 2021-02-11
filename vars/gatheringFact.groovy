fce5a36edb29bbf414c3ebb440a7eb4652908c4b
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 22:30:26 +0100

    test

--- vars/gatheringFact.groovy
+++ vars/gatheringFact.groovy
@@ -1,20 +1,51 @@
 def call(Object formParams) {
-
     println(formParams)
     isBranchDevelp(formParams.branch)
 }
 
-def getBranch(String branch){
+def getBranch(String branch) {
     String result
-    if(result = isBranchDevelp(formParams.branch))
+    String develop = isBranchDevelop(formParams.branch)
+    String feature = isBranchFeature(formParams.branch)
+    String epicfeature = isBranchEpicfeature(formParams.branch)
+    String release = isBranchRelease(formParams.branch)
+    // if(result = isBranchDevelp(formParams.branch))
 
     println(result)
+    println(develop)
+    println(feature)
+    println(epicfeature)
+    println(release)
 }
 
-def isBranchDevelp(String branch){
+String isBranchFeature(String branch) {
     String result
-    if(branch ==~/(.*\/develop\/)|(develop)$/){
-        result = "develop"
+    if (branch ==~ /(.*\/feature\/)|(feature).*$/) {
+        result = 'feature'
+    }
+    return result
+}
+
+String isBranchEpicfeature(String branch) {
+    String result
+    if (branch ==~ /(.*\/epicfeature\/)|(epicfeature).*$/) {
+        result = 'epicfeature'
+    }
+    return result
+}
+
+String isBranchDevelop(String branch) {
+    String result
+    if (branch ==~ /(.*\/develop\/)|(develop)$/) {
+        result = 'develop'
+    }
+    return result
+}
+
+String isBranchRelease(String branch) {
+    String result
+    if (branch ==~ /(.*\/release\/)|(release)$/) {
+        result = 'release'
     }
     return result
 }
