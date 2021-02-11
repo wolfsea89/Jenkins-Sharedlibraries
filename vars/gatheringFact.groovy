efff0440422ac9356f1cdd68eb4047e27fecf2db
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 23:00:47 +0100

    test

--- vars/gatheringFact.groovy
+++ vars/gatheringFact.groovy
@@ -7,6 +7,17 @@ def call(Object formParams) {
     
 }
 
+def getVersion(String branch){
+    String branchPrefix = getBranch(branch)
+    if (branchPrefix ==~/feature|epicfeature|develop/) {
+        def now = new Date()
+        def time1 = now.getMonth()
+        def time2 = now.getYear()
+        def time3 = now.getDay()
+        println("${time1},${time2},${time3}")
+    }
+}
+
 def getBranch(String branch, Object env) {
     String result
     if (branch ==~ /(.*\/feature)|(feature)\/.*$/) {
@@ -23,15 +34,4 @@ def getBranch(String branch, Object env) {
         result = 'master'
     }
     return result
-}
-
-def getVersion(String branch){
-    String branchPrefix = getBranch(branch)
-    if (branchPrefix ==~/feature|epicfeature|develop/) {
-        def now = new Date()
-        def time1 = now.getMonth()
-        def time2 = now.getYear()
-        def time3 = now.getDay()
-        println("${time1},${time2},${time3}")
-    }
 }
