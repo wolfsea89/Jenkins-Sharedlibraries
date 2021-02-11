636bcd3bd16767a5aa2b22f8328c6d5163026765
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 14:32:28 +0100

    test

--- src/org/ci/GatheringInformation.groovy
+++ src/org/ci/GatheringInformation.groovy
@@ -1,9 +1,11 @@
 package org.ci
+
 /* groovylint-disable-next-line ClassJavadoc, CompileStatic, UnnecessaryPublicModifier */
 public class GatheringInformation {
 
     /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
     def getBranchPrefix (String branchName) {
+        print(branchName)
         if (branchName) {
             if (branchName.indexOf('/') > 1) {
                 /* groovylint-disable-next-line DuplicateStringLiteral */
