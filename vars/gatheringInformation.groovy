a6aee019177f9dd4bf4d316aa9b0d580d46942dc
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 15:57:35 +0100

    test

--- vars/gatheringInformation.groovy
+++ vars/gatheringInformation.groovy
@@ -1,5 +1,18 @@
 import org.ci.GatheringInformation
 
-def call() {
-    return new GatheringInformation()
+def branchNamePrefix = ""
+
+// def call() {
+//     return new GatheringInformation()
+// }
+
+def call (String branchName) {
+    if (branchName) {
+        if (branchName.indexOf('/') > 1) {
+            /* groovylint-disable-next-line DuplicateStringLiteral */
+            return branchName.split('/')[1]
+        }
+    } else {
+        error('ERROR: branchName argument not specified ')
+    }
 }
