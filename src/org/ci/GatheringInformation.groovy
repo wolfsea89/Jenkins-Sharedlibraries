2893769ff9827d0343c1c5319685707486b94395
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 16:06:01 +0100

    test

--- src/org/ci/GatheringInformation.groovy
+++ src/org/ci/GatheringInformation.groovy
@@ -1,17 +1,26 @@
 package org.ci
-/* groovylint-disable-next-line ClassJavadoc, CompileStatic, UnnecessaryPublicModifier */
-public class GatheringInformation {
 
-    /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
-    def getBranchPrefix (String branchName) {
+/* groovylint-disable-next-line ClassJavadoc, CompileStatic, UnnecessaryPublicModifier */
+class GatheringInformation implements Serializable {
+
+    def branchPrefix= ""
+    
+    GatheringInformation(branchName){
+        print(branchName)
         if (branchName) {
-            if (branchName.indexOf('/') > 1) {
-                /* groovylint-disable-next-line DuplicateStringLiteral */
-                return branchName.split('/')[1]
-            }
+            // if (branchName.indexOf('/') > 1) {
+                // this.branchPrefix = branchName.split('/')[1]
+        this.branchPrefix = branchName
+            // }
         } else {
             error('ERROR: branchName argument not specified ')
         }
+    }
+
+    /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
+    def getBranchPrefix () {
+        print(this.branchName)
+        return this.branchPrefix
     }
 
 }
