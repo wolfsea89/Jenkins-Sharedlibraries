d4e87a9cd783da75008957ac4fc3a0021547e741
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 22:19:36 +0100

    test

--- vars/gatheringFact.groovy
+++ vars/gatheringFact.groovy
@@ -1,4 +1,11 @@
 def call(Object formParams) {
 
     println(formParams)
+    isBranchDevelp(formParams.branch)
+}
+
+def isBranchDevelp(String branch){
+    if(branch ==~/.*(\/develop\/)$/){
+        println('ok')
+    }
 }
