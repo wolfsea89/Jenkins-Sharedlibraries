962e819b2b3015cb003778722d9be6d268b90cf8
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 17:11:02 +0100

    test

--- src/org/ci/GatheringInformation.groovy
+++ src/org/ci/GatheringInformation.groovy
@@ -8,7 +8,7 @@ class GatheringInformation implements Serializable {
         this.pipeline = pipeline
     }
 
-    public  void setBranchName (String branchName) {
+    public static void setBranchName (String branchName) {
         this.branchName = branchName
     }
 
