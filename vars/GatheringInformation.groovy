acede3ae253383d8ac78d0b9a6c6c22cb5fc9e9b
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-11 14:20:08 +0100

    test

--- vars/GatheringInformation.groovy
+++ vars/GatheringInformation.groovy
@@ -1,19 +1,4 @@
-/* groovylint-disable-next-line ClassJavadoc, CompileStatic, UnnecessaryPublicModifier */
-class GatheringInformation {
-
-    /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
-    def getBranchPrefix (String branchName) {
-        if (branchName) {
-            if (branchName.indexOf('/') > 1) {
-                /* groovylint-disable-next-line DuplicateStringLiteral */
-                return branchName.split('/')[1]
-            }
-        } else {
-            error('ERROR: branchName argument not specified ')
-        }
-    }
-
-}
+import org.ci.GatheringInformation
 
 def call() {
     return new GatheringInformation()
