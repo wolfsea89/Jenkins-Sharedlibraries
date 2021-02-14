9075e4340bb22a4cf65422675c0674ba8e06675b
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-14 20:32:41 +0100

    [Docker CI] - build docker from dockerfile #9 [Docker CI] - publish docker image #10

---
+++ vars/dockerCi.groovy
@@ -0,0 +1,28 @@
+def buildProjects(Object projects, String version){
+
+  for(project in projects){
+    sh("docker build -t ${project.name}:${version} -f ${project.dockerfilePath} --no-cache  .")
+  }
+
+}
+
+def publishBaseImage(Object projects, String version, String dockerRepositoryUrl, String dockerRepositoryName, String dockerCredentialId){
+
+  docker.withRegistry(dockerRepositoryUrl, dockerCredentialId) {
+
+    String projectName
+    String repositoryName
+
+    for(project in projects){
+      projectName = project.name
+      repositoryName = dockerRepositoryName.replace("\${projectName}", projectName)
+
+      sh("docker tag $projectName:$version $repositoryName:$version")
+      sh("docker push $repositoryName:$version")
+
+      // Clean after build
+      sh("docker rmi $repositoryName:$version")
+      sh("docker rmi $projectName:$version")
+    }
+  }
+}
