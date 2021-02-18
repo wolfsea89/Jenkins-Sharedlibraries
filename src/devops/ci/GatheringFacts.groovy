8c538cdf30256afe88f4a39fbe8ae8043b1e0be3
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 19:45:33 +0100

    test

--- src/devops/ci/GatheringFacts.groovy
+++ src/devops/ci/GatheringFacts.groovy
@@ -13,23 +13,35 @@ class GatheringFacts {
     String artifactType 
 
     GatheringFacts(def params, def env){
+        // Params
         this.branchName = params.branch
         this.repositoryUrl = params.repositoryUrl
-        this.manualVersion = params.manualVersion
+        if(params.manualVersion == "" || params.manualVersion ==~ /[0-9]+\.[0-9]+\.[0-9]+$/){
+            this.manualVersion = params.manualVersion
+        } else {
+            throw new IllegalArgumentException('ERROR: Invalid set manual version')
+        }
+
+        // Env
         this.jobName = env.JOB_BASE_NAME
         this.jobBuildNumber = env.BUILD_NUMBER
         this.nodeName = env.NODE_NAME
         this.workspace = env.WORKSPACE
+
+        // Set variables
         isFeature()
         isEpicFeature()
         isDevelop()
         isReleaase()
         isHotfix()
         isMaster()
+
         setArifactType()
+
         if (this.branchNamePrefix == null) {
             throw new IllegalArgumentException('ERROR: Branch name not compatible with gitflow. Expects value (feature/*, epicfeature/*, develop, release, release/X.Y, release/X.Y.0, hotfix, hotfix/X.Y.Z, master)')
         }
+
         
     }
 
@@ -84,5 +96,22 @@ class GatheringFacts {
     @NonCPS
     public String setArifactType(){
         this.artifactType = (this.branchNamePrefix ==~/^release|hotfix$/) ? 'release' : 'snapshot'
+    }
+
+    public String semanticVersion(){
+
+        def now = new Date()
+        version = "${now.format('yyyy.M.d', TimeZone.getTimeZone('UTC'))}"
+
+        if (this.branchNamePrefix ==~ /release|hotfix|master/) {
+            
+            if(this.manualVersion){
+                version = "${manualVersion}"
+            } else {
+                version =~ /[0-9]+\.[0-9]+\.[0-9]+$/
+                version =~ /[0-9]+\.[0-9]+$/ + ".0"
+            }
+        }
+        this.version = version
     }
 }
