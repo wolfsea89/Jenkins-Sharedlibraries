a493cda79251c0a872b4a7a6d2bc4466601b6fa2
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 22:50:56 +0100

    test

--- src/devops/ci/GatheringFacts.groovy
+++ src/devops/ci/GatheringFacts.groovy
@@ -134,6 +134,6 @@ class GatheringFacts {
     }
 
     public void covertApplicationJsonToObject(def jsonString){
-        this.applicationConfiguration = new JsonSlurper().parse(File this.applicationJsonFile)
+        this.applicationConfiguration = new JsonSlurper().parseText(jsonString)
     }
 }
