171b20f14f0f3408b09067ae7c64e663d8bb6916
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 10:13:10 +0100

    test

---
+++ src/devops/ci/GatheringFacts.groovy
@@ -0,0 +1,24 @@
+class GatheringFacts extends Abstractcommand {
+
+    List<Command> list = new ArrayList<Command>()
+
+    List<Command> getCommandList(){
+        return list
+    }
+
+    GatheringFacts setParams(Object params){
+        list.add(new SetParams(params))
+        return this
+    }
+
+    @Override
+    GatheringFacts clone(){
+        try{
+            GatheringFacts cmd = (GatheringFacts) super.clone()
+            cmd.setList((getList().clone() as ArrayList<Command>))
+            return cmd
+        } catch (CloneNotSupportedException e){
+            throw new AssertionError(e)
+        }
+    }
+}   
