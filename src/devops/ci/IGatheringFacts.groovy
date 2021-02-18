e0b871a3747d2827fc9e9ab6eb86b5443077eca1
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 14:12:53 +0100

    test

--- src/devops/ci/IGatheringFacts.groovy
+++ src/devops/ci/IGatheringFacts.groovy
@@ -2,7 +2,9 @@ package devops.ci
 
 interface IGatheringFacts extends Serializable {
     
+    @Override
     GatheringFacts setBranchName(String branchName);
+    
     String getBranchName();
     String getBranchNamePrefix();
 }
