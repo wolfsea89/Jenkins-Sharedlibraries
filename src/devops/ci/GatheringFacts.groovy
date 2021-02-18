5f1bddabcee390018b7e03c22c62942354398be2
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 22:24:40 +0100

    test

--- src/devops/ci/GatheringFacts.groovy
+++ src/devops/ci/GatheringFacts.groovy
@@ -37,7 +37,7 @@ class GatheringFacts {
         this.workspace = env.WORKSPACE
         this.jenkinsScriptDirectory = env.JENKINSFILE_SCRIPTS_DIR
         this.gitCredentialId = env.GIT_CREDS_ID
-        this.applicationJsonFile = this.workspace + '/' + env.APP_CONFIGURATION_JSON_PATH
+        this.applicationJsonFile = env.APP_CONFIGURATION_JSON_PATH
 
         // Set variables
         isFeature()
