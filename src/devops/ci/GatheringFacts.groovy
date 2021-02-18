f38e840256c9d4e450bbe092ec1b7cd1859e98a2
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 23:47:02 +0100

    test

--- src/devops/ci/GatheringFacts.groovy
+++ src/devops/ci/GatheringFacts.groovy
@@ -42,7 +42,7 @@ class GatheringFacts implements Serializable {
         this.workspace = env.WORKSPACE
         this.jenkinsScriptDirectory = env.JENKINSFILE_SCRIPTS_DIR
         this.gitCredentialId = env.GIT_CREDS_ID
-        this.applicationJsonFile = env.WORKSPACE + '/' + env.APP_CONFIGURATION_JSON_PATH
+        this.applicationJsonFile = env.APP_CONFIGURATION_JSON_PATH
 
         // Set variables
         isFeature()
