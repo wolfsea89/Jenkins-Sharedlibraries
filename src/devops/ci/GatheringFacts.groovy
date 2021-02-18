046321dfe3d6c686371480dd5c801c16572ddbaf
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 20:10:03 +0100

    test

--- src/devops/ci/GatheringFacts.groovy
+++ src/devops/ci/GatheringFacts.groovy
@@ -114,7 +114,7 @@ class GatheringFacts {
                 // def match = (this.branchName =~ /[0-9]+\.[0-9]+\.[0-9]+$/)
                 // version = match.find() ? match.group(1) : null
 
-                match = (this.branchName =~ /(.*)([0-9]+\.[0-9]+)$/)
+                def match = (this.branchName =~ /(.*)([0-9]+\.[0-9]+)$/)
                 version = match.group(1)
 
             // }
