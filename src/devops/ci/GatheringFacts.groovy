b177160d3ec0b97aad4cd109bdf7cc576e2f1295
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 12:15:33 +0100

    test

--- src/devops/ci/GatheringFacts.groovy
+++ src/devops/ci/GatheringFacts.groovy
@@ -2,18 +2,42 @@ package devops.ci
 
 class GatheringFacts implements IGatheringFacts {
 
-    private def params
+    private static final defaultBranchPrefix = 'feature'
+
+    private String branchName
+    private String branchNamePrefix
     // GatheringFacts(){
 
     // }
 
-    @Override
-    void setParams(def params){
-        this.params = params
+    public GatheringFacts setBranchName(String branchName){
+        this.branchName = branchName
+        this.setBranchPrefix(branchName)
+        return this
     }
 
-    def getParams(){
-        return this.params
+    public getBranchNamePrefix(){
+        return this.branchName
     }
+
+    private void setBranchPrefix(String branchName){
+
+        if (branchName ==~ /(.*\/feature)|(feature)\/.*$/) {
+            this.branchNamePrefix = 'feature'
+        } else if (branchName ==~ /(.*\/epicfeature)|(epicfeature)\/.*$/) {
+            this.branchNamePrefix = 'epicfeature'
+        } else if (branchName ==~ /(.*\/develop)|(develop)$/) {
+            this.branchNamePrefix = 'develop'
+        } else if (branchName ==~ /(.*\/release|release)(\/([0-9]+\.[0-9]+|[0-9]+\.[0-9]+\.0)|)$/) {
+            this.branchNamePrefix = 'release'
+        } else if (branchName ==~ /(.*\/hotfix|hotfix)(\/([0-9]+\.[0-9]+\.[0-9]+)|)$/) {
+            this.branchNamePrefix = 'hotfix'
+        } else if (branchName ==~ /(.*\/master)|(master)$/) {
+            this.branchNamePrefix = 'master'
+        } else {
+            error('ERROR: Branch name not compatible with gitflow. Expects value (feature/*, epicfeature/*, develop, release, release/X.Y, release/X.Y.0, hotfix, hotfix/X.Y.Z, master)')
+        }
+    }
+
 
 }   
