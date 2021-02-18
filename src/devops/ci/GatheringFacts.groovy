6b8b3f954d1fdb2cf2391d8686038d89f8284225
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-18 19:55:12 +0100

    test

--- src/devops/ci/GatheringFacts.groovy
+++ src/devops/ci/GatheringFacts.groovy
@@ -110,8 +110,8 @@ class GatheringFacts {
             if(this.manualVersion){
                 version = "${manualVersion}"
             } else {
-                version =~ /[0-9]+\.[0-9]+\.[0-9]+$/[ 0 ]​[ 1 ]
-                version =~ /[0-9]+\.[0-9]+$/ + ".0"
+                version = (this.manualVersion =~ /[0-9]+\.[0-9]+\.[0-9]+$/)
+                version = (this.manualVersion =~ /[0-9]+\.[0-9]+$/) + ".0"
             }
         }
         this.version = version
