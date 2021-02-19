13f658a0db24b309c7c92b820f9b607caa686038
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-19 22:34:44 +0100

    test

--- src/devops/ci/GatheringFacts.groovy
+++ src/devops/ci/GatheringFacts.groovy
@@ -63,7 +63,7 @@ class GatheringFacts implements Serializable {
         // }
     }
 
-    public GatheringFacts setParametersFromForm(Parameters parameters){
+    public GatheringFacts setParametersFromForm(Maps parameters){
         this.branchName = parameters.branchName
         this.repositoryUrl = parameters.repositoryUrl
         this.manualVersion = parameters.manualVersion
