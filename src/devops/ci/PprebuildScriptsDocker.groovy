2b0ff6a87de31c574c4fada41d6ab9a82e57e656
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-20 01:14:50 +0100

    test

--- src/devops/ci/PprebuildScriptsDocker.groovy
+++ src/devops/ci/PprebuildScriptsDocker.groovy
@@ -10,11 +10,15 @@ class PprebuildScriptsDocker{
     this.pipeline = pipeline
   }
 
-  def setVersion(List applications){
+  def setVersion(List applications, String version){
     for(docker_project in applications){
+
       def file = readFile file: docker_project.dockerfilePath
-      file = file.replaceAll('\\$\\{jenkins_include_version\\}', facts.version.semanticVersionWithBuildNumber)
+      
+      file = file.replaceAll('\\$\\{jenkins_include_version\\}', version)
+      
       writeFile(file: docker_project.dockerfilePath, text: file)
+      
       println("Set version in file ${docker_project.dockerfilePath}")
     }
   }
