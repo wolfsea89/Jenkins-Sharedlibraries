422cf4abafb17d127f2439456b98d573d03203d2
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-20 01:20:00 +0100

    test

--- src/devops/ci/PrebuildScriptsDocker.groovy
+++ src/devops/ci/PrebuildScriptsDocker.groovy
@@ -13,11 +13,11 @@ class PrebuildScriptsDocker implements Serializable {
   def setVersionFiles(Object applications, String version){
     for(docker_project in applications){
 
-      def file = readFile file: docker_project.dockerfilePath
+      def file = this.readFile file: docker_project.dockerfilePath
       
       file = file.replaceAll('\\$\\{jenkins_include_version\\}', version)
       
-      writeFile(file: docker_project.dockerfilePath, text: file)
+      this.writeFile(file: docker_project.dockerfilePath, text: file)
       
       println("Set version in file ${docker_project.dockerfilePath}")
     }
