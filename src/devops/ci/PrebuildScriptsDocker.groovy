60df9a14d016f9e2d8efd367c33a33feaddb287c
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-20 01:22:25 +0100

    test

--- src/devops/ci/PrebuildScriptsDocker.groovy
+++ src/devops/ci/PrebuildScriptsDocker.groovy
@@ -20,7 +20,7 @@ class PrebuildScriptsDocker implements Serializable {
       
       this.pipeline.writeFile(file: docker_project.dockerfilePath, text: file)
       
-      println("Set version in file ${docker_project.dockerfilePath}")
+      print("Set version in file ${docker_project.dockerfilePath}")
     }
   }
 }
